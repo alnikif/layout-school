7bf06c5c94f4390edb4a4f13aef8908f96f28405
alnikif <nikiforiakoleksandr@gmail.com>
2023-06-29 12:04:25 +0200

    fix media

--- js/app.js
+++ js/app.js
@@ -3,12 +3,21 @@ document.addEventListener('DOMContentLoaded', function(){
     const iconMenu = bgMenu.querySelector('i');
     const menu = document.querySelector('.menu');
     iconMenu.addEventListener('click', function(){
+        menu.classList.toggle('mobileMenu');
         if(this.classList[1] == 'fa-bars'){
             this.classList.remove('fa-bars');
             this.classList.add('fa-times');
+            menu.style.display = 'block';
         }else {
             this.classList.remove('fa-times');
             this.classList.add('fa-bars');
+            menu.style.display = 'none';
+        }
+    })
+    window.addEventListener('resize', function(){
+        if(window.innerWidth > 768){
+            menu.classList.remove('mobileMenu');
+            menu.style.display = '';
         }
     })
 })
